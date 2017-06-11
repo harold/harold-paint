6f29519e2d80435b7175c8ed6be15fee17a6f52f
Harold <hhausman@gmail.com>
2017-06-11 00:44:25 -0600

    [color-picker] First cut at some interactivity, RGB only so far.

--- src/cljs/harold_paint/color_picker.cljs
+++ src/cljs/harold_paint/color_picker.cljs
@@ -1,5 +1,14 @@
 (ns harold-paint.color-picker
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [goog.events :as events])
+  (:import [goog.events EventType]))
+
+(defn- filled-circle
+  [canvas-ctx x y r color]
+  (aset canvas-ctx "fillStyle" color)
+  (.beginPath canvas-ctx)
+  (.arc canvas-ctx x y r 0 (* 2 Math/PI))
+  (.fill canvas-ctx))
 
 (defn- render
   [ctx]
@@ -7,8 +16,43 @@
         [r g b] color
         w (-> canvas-ctx .-canvas .-clientWidth)
         h (-> canvas-ctx .-canvas .-clientHeight)]
+    (dotimes [i 16]
+      (aset canvas-ctx "fillStyle" (str "rgb("(* 16 i)","g","b")"))
+      (.fillRect canvas-ctx 0 (* (- 15 i) (quot h 16)) (quot w 9) (quot h 16)))
+    (filled-circle canvas-ctx (quot w 18) (- h (* 1.0 h (/ r 255))) 6 "#000000")
+    (filled-circle canvas-ctx (quot w 18) (- h (* 1.0 h (/ r 255))) 4 "#ffffff")
+    (dotimes [i 16]
+      (aset canvas-ctx "fillStyle" (str "rgb("r","(* 16 i)","b")"))
+      (.fillRect canvas-ctx (quot w 9) (* (- 15 i) (quot h 16)) (quot w 9) (quot h 16)))
+    (filled-circle canvas-ctx (* 3 (quot w 18)) (- h (* 1.0 h (/ g 255))) 6 "#000000")
+    (filled-circle canvas-ctx (* 3 (quot w 18)) (- h (* 1.0 h (/ g 255))) 4 "#ffffff")
+    (dotimes [i 16]
+      (aset canvas-ctx "fillStyle" (str "rgb("r","g","(* 16 i)")"))
+      (.fillRect canvas-ctx (* 2 (quot w 9)) (* (- 15 i) (quot h 16)) (quot w 9) (quot h 16)))
+    (filled-circle canvas-ctx (* 5 (quot w 18)) (- h (* 1.0 h (/ b 255))) 6 "#000000")
+    (filled-circle canvas-ctx (* 5 (quot w 18)) (- h (* 1.0 h (/ b 255))) 4 "#ffffff")
     (aset canvas-ctx "fillStyle" (str "rgb("r","g","b")"))
-    (.fillRect canvas-ctx 0 0 w h)))
+    (.fillRect canvas-ctx (quot w 3) 0 (quot w 3) h)))
+
+(defn- on-click
+  [ctx e]
+  (let [state* (:state* ctx)
+        {:keys [canvas-ctx color]} @state*
+        [r g b] color
+        w (-> canvas-ctx .-canvas .-clientWidth)
+        h (-> canvas-ctx .-canvas .-clientHeight)
+        x (.-offsetX e)
+        color-index (cond
+                      (< 0 x (quot w 9)) 0
+                      (< (quot w 9) x (* 2 (quot w 9))) 1
+                      (< (* 2 (quot w 9)) x (* 3 (quot w 9))) 2
+                      :else nil)
+        y (.-offsetY e)
+        p (- 1.0 (/ y h))]
+    (when color-index
+      (swap! state* assoc
+             :color (assoc color color-index (int (* p 255))))
+      (render ctx))))
 
 (defn color-picker
   [props]
@@ -18,13 +62,14 @@
     (reagent/create-class
      {:component-did-mount
       (fn [this]
-        (swap! (:state* ctx) assoc
-               :canvas-ctx (-> (reagent/dom-node this)
-                               (.querySelectorAll "canvas")
-                               (aget 0)
-                               (.getContext "2d")))
+        (let [canvas (-> (reagent/dom-node this)
+                         (.querySelectorAll "canvas")
+                         (aget 0))]
+          (swap! (:state* ctx) assoc
+                 :canvas-ctx (.getContext canvas "2d")
+                 :mouse-down-key (events/listen canvas (.-MOUSEDOWN EventType) (partial on-click ctx))))
         (render ctx))
       :reagent-render
       (fn [props]
         [:div.color-picker
-         [:canvas {:width 128 :height 128}]])})))
+         [:canvas {:width 384 :height 128}]])})))
