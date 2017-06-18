ee3ebc3a56644ca5f02eda727d47cfe274eec93f
Harold <hhausman@gmail.com>
2017-06-17 18:02:20 -0600

    [color-picker] First cut at rgb to hsv and back.

--- src/cljs/harold_paint/color_picker.cljs
+++ src/cljs/harold_paint/color_picker.cljs
@@ -3,6 +3,78 @@
             [goog.events :as events])
   (:import [goog.events EventType]))
 
+;; function hsvToRgb(h, s, v) {
+;;   var r, g, b;
+;;   var i = Math.floor(h * 6);
+;;   var f = h * 6 - i;
+;;   var p = v * (1 - s);
+;;   var q = v * (1 - f * s);
+;;   var t = v * (1 - (1 - f) * s);
+;;   switch (i % 6) {
+;;     case 0: r = v, g = t, b = p; break;
+;;     case 1: r = q, g = v, b = p; break;
+;;     case 2: r = p, g = v, b = t; break;
+;;     case 3: r = p, g = q, b = v; break;
+;;     case 4: r = t, g = p, b = v; break;
+;;     case 5: r = v, g = p, b = q; break;
+;;   }
+;;   return [ r * 255, g * 255, b * 255 ];
+;; }
+
+(defn hsv->rgb
+  "hsv in [0,1] -> rgb in [0,255]"
+  [h s v]
+  (let [i (Math/floor (* h 6))
+        f (* h (- 6 i))
+        p (* v (- 1 s))
+        q (* v (- 1 (* f s)))
+        t (* v (- 1 (* (- 1 f) s)))]
+    (->> (condp = (mod i 6)
+           0 [v t p]
+           1 [q v p]
+           2 [p v t]
+           3 [p q v]
+           4 [t p v]
+           5 [v p q])
+         (mapv #(* 255.0 %)))))
+
+;; function rgbToHsv(r, g, b) {
+;;   r /= 255, g /= 255, b /= 255;
+;;   var max = Math.max(r, g, b), min = Math.min(r, g, b);
+;;   var h, s, v = max;
+;;   var d = max - min;
+;;   s = max == 0 ? 0 : d / max;
+;;   if (max == min) {
+;;     h = 0; // achromatic
+;;   } else {
+;;     switch (max) {
+;;       case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+;;       case g: h = (b - r) / d + 2; break;
+;;       case b: h = (r - g) / d + 4; break;
+;;     }
+;;     h /= 6;
+;;   }
+;;   return [ h, s, v ];
+;; }
+
+(defn rgb->hsv
+  "rgb in [0,255] -> hsv -> [0,1]"
+  [r g b]
+  (let [[r g b] (map #(/ % 255.0) [r g b])
+        maximum (max r g b)
+        minimum (min r g b)
+        v maximum
+        d (- maximum minimum)
+        s (if (zero? maximum) 0 (/ d maximum))
+        h (if (= maximum minimum)
+            0
+            (/ (condp = maximum
+                 r (+ (/ (- g b) d) (if (< g b) 6 0))
+                 g (+ (/ (- b r) d) 2)
+                 b (+ (/ (- r g) d) 4))
+               6))]
+    [h s v]))
+
 (defn- filled-circle
   [canvas-ctx x y r color]
   (aset canvas-ctx "fillStyle" color)
